632f90107e49053a78c36fb354543316d526c705
aktot <lord1996@gmail.com>
2016-06-16 16:56:36 +0300

    added Autorizathion and Signing up

--- Project_fbi/Project_fbi/Controllers/HomeController.cs
+++ Project_fbi/Project_fbi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_fbi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,40 @@ namespace Project_fbi.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+        //[Route("{index}/{id}/{*allTheRest}")]
+        [HttpGet]
+        public ActionResult Autorizathion()
+            {
+                return View();
+            }
+         [HttpPost]
+         public ActionResult Autorizathion(UserModel user)
+            {
+                if (ModelState.IsValid)
+                {
+                    return View("MainView", user);
+                }
+                else
+                    return View("FailedAuto");
+            }
+
+        [HttpGet]
+        public ActionResult SignUp()
         {
-            return View();
+             return View();
         }
+        [HttpPost]
+        public ActionResult SignUp(UserModel user)
+        {
+            {
+                if (ModelState.IsValid)
+                {
+                    return View("Autorizathion");
+                }
+                else
+                    return View("SignUp");
+            }
+        }
+
     }
 }
---
+++ Project_fbi/Project_fbi/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project_fbi.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Будь ласка, введіть свій емейл")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Ви ввели некоректний email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Будь ласка, введіть пароль")]
+        public string Password { get; set; }
+
+    }
+}
