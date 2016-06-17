08d002f00a3e496f06faf2f99eae0b54d3eb22bd
aktot <lord1996@gmail.com>
2016-06-18 00:54:40 +0300

    Modified Signing up

--- Project_fbi/Project_fbi/Models/UserModel.cs
+++ Project_fbi/Project_fbi/Models/UserModel.cs
@@ -8,6 +8,16 @@ namespace Project_fbi.Models
 {
     public class UserModel
     {
+        [Required(ErrorMessage = "Будь ласка, введіть своє ім'я")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Будь ласка, введіть своє прізвище")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Будь ласка, введіть свій телефон")]
+        public string Phone { get; set; }
+
         [Required(ErrorMessage = "Будь ласка, введіть свій емейл")]
         [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Ви ввели некоректний email")]
         public string Email { get; set; }
