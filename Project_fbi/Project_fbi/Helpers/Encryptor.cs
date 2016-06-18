f8ea0136d675cbba73b47814e838c7ff0ff04c50
Andriy Kavetsky <AndriyKavetsky@gmail.com>
2016-06-18 17:08:21 +0300

    added new method to encryptor

--- Project_fbi/Project_fbi/Helpers/Encryptor.cs
+++ Project_fbi/Project_fbi/Helpers/Encryptor.cs
@@ -6,58 +6,69 @@ using System.Drawing;
 namespace Project_fbi.Helpers
 {
     public class Encryptor
-    {
-            public enum State
-            {
-                Hiding,
-                Filling_With_Zeros
-            };
-        public static Bitmap Encrypt(string text, Bitmap bmp)
+    {          
+        public int[] ClearLeastSugnificantBitColor(Color color)
         {
-            State state = State.Hiding;
+            int[] resColor = new int[3];
+            resColor[0] = color.R - color.R % 2;
+            resColor[1] = color.G - color.G % 2;
+            resColor[2] = color.B - color.B % 2;
+            return resColor;
+        }
+        public Bitmap Encrypt(string text, Bitmap bmp)
+        {
+            bool isEncrypting = true;
+
             int charIndex = 0;
+
             int charValue = 0;
+
             long pixelElementIndex = 0;
-            int zeros = 0, R = 0, G = 0, B = 0;
-            
+
+            int zeros = 0;
+
+            int R = 0, G = 0, B = 0;
+
             for (int i = 0; i < bmp.Height; i++)
             {
                 for (int j = 0; j < bmp.Width; j++)
                 {
                     Color pixel = bmp.GetPixel(j, i);
-                    
-                    R = pixel.R - pixel.R % 2;
-                    G = pixel.G - pixel.G % 2;
-                    B = pixel.B - pixel.B % 2;
+
+                    int[] clearValue = ClearLeastSugnificantBitColor(pixel);
+
+                    R = clearValue[0];
+                    G = clearValue[1];
+                    B = clearValue[2];
+
                     for (int n = 0; n < 3; n++)
                     {
                         if (pixelElementIndex % 8 == 0)
                         {
-                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            if (!isEncrypting && zeros == 8)
                             {
                                 if ((pixelElementIndex - 1) % 3 < 2)
                                 {
                                     bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
                                 }
-                                
                                 return bmp;
                             }
-                            
+
                             if (charIndex >= text.Length)
                             {
-                                state = State.Filling_With_Zeros;
+                                isEncrypting = false;
                             }
                             else
                             {
                                 charValue = text[charIndex++];
                             }
                         }
-                        
+
                         switch (pixelElementIndex % 3)
                         {
                             case 0:
                                 {
-                                    if (state == State.Hiding)
+                                    if (isEncrypting)
                                     {
                                         R += charValue % 2;
                                         charValue /= 2;
@@ -66,20 +77,18 @@ namespace Project_fbi.Helpers
                                 break;
                             case 1:
                                 {
-                                    if (state == State.Hiding)
+                                    if (isEncrypting)
                                     {
                                         G += charValue % 2;
-
                                         charValue /= 2;
                                     }
                                 }
                                 break;
                             case 2:
                                 {
-                                    if (state == State.Hiding)
+                                    if (isEncrypting)
                                     {
                                         B += charValue % 2;
-
                                         charValue /= 2;
                                     }
 
@@ -90,7 +99,7 @@ namespace Project_fbi.Helpers
 
                         pixelElementIndex++;
 
-                        if (state == State.Filling_With_Zeros)
+                        if (!isEncrypting)
                         {
                             zeros++;
                         }
@@ -100,8 +109,8 @@ namespace Project_fbi.Helpers
 
             return bmp;
         }
-
-        public static string Decrypt(Bitmap bmp)
+        
+        public string Decrypt(Bitmap bmp)
             {
                 int colorUnitIndex = 0;
                 int charValue = 0;
@@ -155,7 +164,7 @@ namespace Project_fbi.Helpers
                 return extractedText;
             }
 
-            public static int reverseBits(int n)
+            public int reverseBits(int n)
             {
                 int result = 0;
 
