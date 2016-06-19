19edbfb691300b81334ba58a15c7a1158a92f445
aktot <lord1996@gmail.com>
2016-06-19 23:11:59 +0300

    added Decryption

---
+++ Project_fbi/Project_fbi/Models/Images.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace Project_fbi.Models
+{
+    public class Images
+    {
+        public string Text { get; set; }
+
+    }
+}
